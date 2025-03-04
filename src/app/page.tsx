ea6e6ebb6c8dc52e67f4ebb5d08f581a2b5fff50
tapader13 <minhajtapader0@gmail.com>
2025-03-04 14:31:24 -0500

    remove second hero part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className='font-young'>
       <Hero />
-      <SecondHero />
+      {/* <SecondHero /> */}
       <NewRelease />
       <ShirtItems />
       <BraSpotlite />
